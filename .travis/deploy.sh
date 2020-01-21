1f4934c0635526b1ceeac3b42a3ce623a356c631
Konasov Dmitriy <d.konasov@yandex.ru>
2020-01-22 00:26:21 +0300

    fixed uploading artifacts in deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 ssh -t dkonasov@konasov.space "rm -rf ~/dskblog/dskblog-frontend && mkdir ~/dskblog/dskblog-frontend"
-scp -r dkonasov@konasov.space:/home/dkonasov/dskblog/dskblog-frontend dist
+scp -r dist dkonasov@konasov.space:/home/dkonasov/dskblog/dskblog-frontend
