9cf19edee56d3c129e69f8c9cf230dda0f7ae718
Konasov Dmitriy <d.konasov@yandex.ru>
2020-01-24 00:45:32 +0300

    #1 добавлен перезапуск docker-compose в скрипт деплоя

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
+ssh -t dkonasov@konasov.space "docker-compose down"
 ssh -t dkonasov@konasov.space "rm -rf ~/dskblog/dskblog-frontend && mkdir ~/dskblog/dskblog-frontend"
 scp -r dist dkonasov@konasov.space:/home/dkonasov/dskblog/dskblog-frontend
+ssh -t dkonasov@konasov.space "docker-compose up -d"
